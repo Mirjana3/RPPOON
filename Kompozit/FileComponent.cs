90a1d0d9ae7fba483fb66aa590fc5b66fea59bd8
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2024-01-30 21:42:24 +0100

    Create FileComponent.cs

---
+++ Kompozit/FileComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kompozit
+{
+    public abstract class FileComponent
+    {
+        public abstract void Display();
+    }
+}
