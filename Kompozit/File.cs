fd0a6bdf687cedefb4c3b1c998d70fbb8d699cb8
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2024-01-30 21:41:57 +0100

    Create File.cs

---
+++ Kompozit/File.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kompozit
+{
+    public class File :FileComponent
+    {
+        private string Name;
+
+        public File (string name)
+        {
+            Name = name;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"File: {Name}");
+        }
+    }
+}
