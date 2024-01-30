e6ebce27229fe91be81d7ba5870e2ea7dbdeb845
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2024-01-30 21:39:26 +0100

    Create Beef.cs

---
+++ Dekorater/Beef.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dekorater
+{
+    public class Beef
+    {
+        public void AddBeef()
+        {
+            Console.WriteLine("Add Beef");
+        }
+    }
+}
