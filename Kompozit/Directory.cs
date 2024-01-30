8e32d3ad98d92131a4eff5f160f258e0623f3fc4
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2024-01-30 21:41:36 +0100

    Create Directory.cs

---
+++ Kompozit/Directory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kompozit
+{
+    public class Directory : FileComponent
+    {
+        private string Name;
+        private List<FileComponent> Files;
+
+        public Directory(string name)
+        {
+            Name = name;
+            Files = new List<FileComponent>();
+        }
+
+        public void AddComponent(FileComponent component)
+        {
+            Files.Add(component);
+        }
+
+        public void RemoveComponent(FileComponent component)
+        {
+            Files.Remove(component);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Directory: {Name}");
+            foreach (var child in Files)
+            {
+                child.Display();
+            }
+        }
+    }
+}
