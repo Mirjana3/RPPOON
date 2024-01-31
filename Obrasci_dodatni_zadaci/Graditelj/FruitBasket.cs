2d1214471e52d1c5aad91661695fe2566f7666be
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2024-01-31 19:48:13 +0100

    Create FruitBasket.cs

---
+++ Obrasci_dodatni_zadaci/Graditelj/FruitBasket.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obrasci_dodatni_zadaci
+{
+    public class FruitBasket
+    {
+        public Dictionary<Fruit, int> Contents { get; } = new Dictionary<Fruit, int>();
+    }
+}
