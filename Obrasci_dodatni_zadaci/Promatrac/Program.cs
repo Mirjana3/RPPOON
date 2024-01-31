a2a5098445d8877442c4d31ca9effb378acea1ed
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2024-01-31 19:26:22 +0100

    Create Program.cs

---
+++ Obrasci_dodatni_zadaci/Promatrac/Program.cs
@@ -0,0 +1,25 @@
+using System;
+
+public enum Weather { Synny, Rainy, Snowy}
+
+namespace Obrasci_dodatni_zadaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherForecastProvider weatherProvider = new WeatherForecastProvider();
+            WeatherStatistics weatherStatistics = new WeatherStatistics();
+            WeatherDisplay weatherDisplay = new WeatherDisplay();
+
+            weatherProvider.Register(weatherStatistics);
+            weatherProvider.Register(weatherDisplay);
+
+            weatherProvider.PeriodicUpdate();
+
+            weatherProvider.Unregister(weatherStatistics);
+
+            weatherProvider.PeriodicUpdate();
+        }
+    }
+}
