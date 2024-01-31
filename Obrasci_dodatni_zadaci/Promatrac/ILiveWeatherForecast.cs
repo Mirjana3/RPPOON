908b8f46892e5780eba44331c257b72a333d910f
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2024-01-31 19:46:46 +0100

    Add files via upload

---
+++ Obrasci_dodatni_zadaci/Promatrac/ILiveWeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obrasci_dodatni_zadaci
+{
+    public interface ILiveWeatherForecast
+    {
+        public void Register(IWeatherProcessor processor);
+        public void Unregister(IWeatherProcessor processor);
+        public void Notify();
+    }
+}
