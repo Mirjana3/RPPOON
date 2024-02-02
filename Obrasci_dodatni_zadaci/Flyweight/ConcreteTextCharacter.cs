9571c4c38af736eaaf9add13baedca933442b961
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2024-02-02 10:57:17 +0100

    Add files via upload

---
+++ Obrasci_dodatni_zadaci/Flyweight/ConcreteTextCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obrasci_dodatni_zadaci
+{
+    public class ConcreteTextCharacter : ITextCharacter
+    {
+        private char character;
+        private string font;
+        private ConsoleColor color;
+        public ConcreteTextCharacter(char character, string font, ConsoleColor color)
+        {
+            this.character = character;
+            this.font = font;
+            this.color = color;
+        }
+        public void Display(int fontSize)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine($"Character: {character}, Font: {font}, Size: {fontSize}");
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ Obrasci_dodatni_zadaci/Flyweight/TextCharacterFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obrasci_dodatni_zadaci
+{
+    public class TextCharacterFactory
+    {
+        private Dictionary<char, ITextCharacter> characters = new Dictionary<char, ITextCharacter>();
+        public ITextCharacter GetCharacter(char character, string font, ConsoleColor color)
+        {
+            if (!characters.ContainsKey(character))
+            {
+                characters[character] = new ConcreteTextCharacter(character, font, color);
+            }
+            return characters[character];
+        }
+
+    }
+}
