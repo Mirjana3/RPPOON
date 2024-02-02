c2cfc043d8b369a68ceffd36805c9549235e52d4
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2024-02-02 11:07:03 +0100

    Add files via upload

---
+++ Obrasci_dodatni_zadaci/Kompozit/Department.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obrasci_dodatni_zadaci
+{
+    public class Department : IOrganizationComponent
+    {
+        private string name;
+        private List<IOrganizationComponent> members = new List<IOrganizationComponent>();
+        public Department(string name)
+        {
+            this.name = name;
+        }
+        public void Add(IOrganizationComponent component)
+        {
+            members.Add(component);
+        }
+
+        public void Remove(IOrganizationComponent component)
+        {
+            members.Remove(component);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Department: {name}");
+            foreach (var member in members)
+            {
+                member.Display();
+            }
+        }
+
+    }
+}
---
+++ Obrasci_dodatni_zadaci/Kompozit/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obrasci_dodatni_zadaci
+{
+    public class Employee : IOrganizationComponent
+    {
+        private string name;
+        private string position;
+        public Employee(string name, string position)
+        {
+            this.name = name;
+            this.position = position;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Employee: {name}, Position: {position}");
+        }
+
+    }
+}
