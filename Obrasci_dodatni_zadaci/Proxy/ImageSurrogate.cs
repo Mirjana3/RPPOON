956a1103662ee261725f484bbc0fb1c3a4da2a32
Mirjana3 <156024390+Mirjana3@users.noreply.github.com>
2024-02-02 10:49:42 +0100

    Add files via upload

---
+++ Obrasci_dodatni_zadaci/Proxy/ImageSurrogate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obrasci_dodatni_zadaci
+{
+    public class ImageSurrogate : IImage
+    {
+        private string filename;
+        private RealImage image;
+        public ImageSurrogate(string filename)
+        {
+            this.filename = filename;
+        }
+        public void Display()
+        {
+            if (image == null)
+            {
+                image = new RealImage(filename);
+            }
+            image.Display();
+        }
+
+    }
+}
---
+++ Obrasci_dodatni_zadaci/Proxy/RealImage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obrasci_dodatni_zadaci
+{
+    public class RealImage :IImage
+    {
+        private string filename;
+        public RealImage(string filename)
+        {
+            this.filename = filename;
+            LoadImageFromDisk();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Display image: {filename}");
+        }
+
+        private void LoadImageFromDisk()
+        {
+            Console.WriteLine($"Loading image from disk: {filename}");
+        }
+    }
+}
